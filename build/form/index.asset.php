186cce1826e954ecbfbd0d52efbe03d437ca6c1b
Iqbal Hossain <iqbal.212015026@green.ac.bd>
2025-01-30 21:47:29 +0600

    update

--- build/form/index.asset.php
+++ build/form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '387f3a8d6eb426a19e7c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5684ab17a43db45766b8');
